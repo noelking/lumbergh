2d9fe1d5ee5156813f258c481553074c7c05c0bc
Noel King <noelking@gmail.com>
2013-01-07 15:43:52 +0000

    Added functionality to retrieve the cloud host info

--- JCloud-Chaos/grails-app/controllers/org/jcloudchaos/web/domain/VirtualMachineController.groovy
+++ JCloud-Chaos/grails-app/controllers/org/jcloudchaos/web/domain/VirtualMachineController.groovy
@@ -1,19 +1,40 @@
 package org.jcloudchaos.web.domain
 
-import org.springframework.dao.DataIntegrityViolationException
-import org.jclouds.compute.*
+import org.jclouds.ContextBuilder;
+import org.jclouds.compute.ComputeService;
+import org.jclouds.compute.ComputeServiceContext;
+import org.jclouds.compute.domain.ComputeMetadata;
 
 
 class VirtualMachineController {
-	
-    def scaffold = true
-	
+
+	def scaffold = true
+
 	def list(Integer max) {
 		params.max = Math.min(max ?: 10, 100)
-		[virtualMachineInstanceList: VirtualMachine.list(params), virtualMachineInstanceTotal: VirtualMachine.count()]
+		List<VirtualMachine> vms = connectToClouds()
+		[virtualMachineInstanceList: vms, virtualMachineInstanceTotal: vms.size()]
 	}
-	
+
 	def connectToClouds(){
-		ComputeServiceContext context = new ComputeServiceContextFactory().cresateContext("terremark", user, password);
+		
+		List<VirtualMachine> vms = new ArrayList<VirtualMachine>()
+		ComputeServiceContext context = ContextBuilder.newBuilder("cloudstack")
+				.endpoint("http://172.18.48.180:8096/")
+				.credentials("ABsn9xnC-DskZ__6cQm_XNhusUKWE-8jg4-yjP6CwGf_C_lxxaC1B8guNeUGZDY_kLMKsWYVHKTF3FjpYUdT2Q", "ABsn9xnC-DskZ__6cQm_XNhusUKWE-8jg4-yjP6CwGf_C_lxxaC1B8guNeUGZDY_kLMKsWYVHKTF3FjpYUdT2Q")
+				.buildView(ComputeServiceContext.class)
+
+		ComputeService client = context.getComputeService();
+		
+		for (ComputeMetadata node : client.listNodes()) {
+			VirtualMachine vm = new VirtualMachine()
+			vm.setHostName(node.getName())
+			vm.setIgnore(false)
+			vm.setDescription("adfadfsdf")
+			vm.setTemplateId(node.getId())
+			vm.setTemplateName("blah")
+			vms.add(vm)
+		}
+		vms
 	}
 }
